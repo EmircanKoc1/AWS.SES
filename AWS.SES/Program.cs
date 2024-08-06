61e7ab16d70c1b094ab1e3072418c2b222497d96
Emircan <emircandv787@gmail.com>
2024-08-06 22:49:10 +0300

    send email endpoint was defined using minimal api

--- AWS.SES/Program.cs
+++ AWS.SES/Program.cs
@@ -22,6 +22,52 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+app.MapPost("/send-mail", async (
+    [FromServices] IAmazonSimpleEmailServiceV2 _amazonSimpleEmailService,
+    [FromBody] MailDto mailDto) =>
+{
+    var charSet = "UTF-8";
+
+    var sendEmailRequest = new SendEmailRequest()
+    {
+        FromEmailAddress = mailDto.FromEmail,
+        Destination = new Destination()
+        {
+            ToAddresses = mailDto.toEmail.ToList()
+        },
+        Content = new EmailContent()
+        {
+            Simple = new Message()
+            {
+                Subject = new Content()
+                {
+                    Data = mailDto.subject,
+                    Charset = charSet
+                },
+                Body = new Body()
+                {
+                    Html = new Content()
+                    {
+                        Data = mailDto.htmlContent,
+                        Charset = charSet
+                    },
+                    Text = new Content()
+                    {
+                        Data = mailDto.textContent,
+                        Charset = charSet
+                    }
+                }
+            }
+        }
+
+
+    };
+
+    var sendEmailResponse = await _amazonSimpleEmailService.SendEmailAsync(sendEmailRequest);
+
+
+    return Results.Ok(sendEmailResponse);
+});
 
 
 app.Run();
